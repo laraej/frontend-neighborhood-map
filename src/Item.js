65af629bac9e8e2f799c41825d19dc55b8df8498
Lara <lara.ej@gmail.com>
2018-08-22 22:07:54 +0100

    Handling enter key

--- src/Item.js
+++ src/Item.js
@@ -5,15 +5,21 @@ class Item extends React.Component {
     super(props);
 
     this.onClick = this.onClick.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
   onClick() {
     this.props.onSelect(this.props.place);
+  }
+  onKeyUp(event) {
+    if (event.key === 'Enter')
+      this.props.onSelect(this.props.place);
   }
   render() {
     const selected = this.props.selectedPlace && this.props.selectedPlace.title === this.props.place.title;
 
     return (
-      <li className={ selected ? 'selected' : '' } onClick={ this.onClick } tabIndex={ 0 }>{ this.props.place.title }</li>
+      <li className={ selected ? 'selected' : '' } onClick={ this.onClick }
+          tabIndex={ 0 } onKeyUp={ this.onKeyUp }>{ this.props.place.title }</li>
     )
   }
 }
